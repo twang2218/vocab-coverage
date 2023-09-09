7b0248e71aac726424599fa699ed166032201bfc
Tao Wang <twang2218@gmail.com>
2023-09-09 20:24:08 +1000

    Bump version to 0.9
    Signed-off-by: Tao Wang <twang2218@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vocab-coverage',
-    version='0.8',
+    version='0.9',
     packages=find_packages(),
     install_requires=[
         'Pillow',
